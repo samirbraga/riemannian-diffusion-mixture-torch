6d853c58ebd8dbed42fb458c45cb988f0cc6ecb9
samirbraga <samirchavess@gmail.com>
2025-11-24 12:21:35 -0300

    feat: update forward methods to include sequence length handling and adjust drift calculations in various modules

--- foldingdiff/bert_for_diffusion.py
+++ foldingdiff/bert_for_diffusion.py
@@ -5,11 +5,28 @@ from foldingdiff.utils import get_bert_attn_tensors
 
 
 class BertForDiffusion(BertForDiffusionBase):
-    def forward(self, inputs: torch.Tensor, timestep: torch.Tensor, manifold):
-        inputs_2d = inputs.view(inputs.shape[0], -1, 12)
-        attn_mask, position_ids, masked_inputs = get_bert_attn_tensors(128, inputs_2d)
-        outputs = super().forward(masked_inputs, timestep, attn_mask, position_ids)
-        outputs = outputs[:, :inputs_2d.shape[1], :]
-        outputs = outputs.reshape(inputs.shape[0], -1)
+    
+    # def forward(self, inputs: torch.Tensor, timestep: torch.Tensor, manifold):
+    #     inputs_2d = inputs.view(inputs.shape[0], -1, 12)
+    #     attn_mask, position_ids, masked_inputs = get_bert_attn_tensors(128, inputs_2d)
+    #     outputs = super().forward(masked_inputs, timestep, attn_mask, position_ids)
+    #     outputs = outputs[:, :inputs_2d.shape[1], :]
+    #     outputs = outputs.reshape(inputs.shape[0], -1)
+    #     drift = manifold.to_tangent(outputs, inputs)
+    #     return drift
+    
+    def forward(self, inputs: torch.Tensor, timestep: torch.Tensor, manifold, seq_len):
+        bs = inputs.shape[0]
+        pad = inputs.shape[1] // 12
+        inputs_2d = inputs.view(inputs.shape[0], -1, 12)   
+            
+        attn_mask = torch.zeros(size=(bs, pad), device=inputs.device)
+        l = min(pad, seq_len)
+        attn_mask[:, :l] = 1.0
+
+        position_ids = torch.arange(start=0, end=pad, step=1, dtype=torch.long, device=inputs.device)
+        
+        outputs = super().forward(inputs_2d, timestep, attn_mask, position_ids)
+        outputs = outputs.reshape(bs, -1)
         drift = manifold.to_tangent(outputs, inputs)
         return drift
